7eb66ac433a5e574f311bb8c26ad0342479050ae
Zylf07 <93834480+Zylf07@users.noreply.github.com>
2023-11-16 10:10:50 +0800

    first commit

---
+++ main/db_conn.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "KristelMae07";
+  $database = "recordsapp";
+
+  // Create connection
+  $connection = new mysqli($servername, $username, $password, $database);
+        
+?>
